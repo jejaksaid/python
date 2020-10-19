448bc28ce6357948ad056e0f3fe6731568e2e85f
jejaksaid <mail@saidramadhan.com>
2020-10-20 01:59:52 +0700

    2010

--- proyek-iseng/url-shortener.py
+++
@@ -1,5 +0,0 @@
-from pyshorteners import Shortener
-
-url = input("Shorten")
-s = pyshorteners.Shortener().tinyurl.short(url)
-print("Result: ", s)
--- pyshort.py
+++ pyshort.py
@@ -1,5 +1,17 @@
+# importing tkinter
+from tkinter import *
+# importing pyshorteners
 import pyshorteners
+
+# initializing root
+root = Tk()
+
+# setting geometry
+root.geometry("400x300")
+
 
 url = input("Enter your URL")
 s = pyshorteners.Shortener().tinyurl.short(url)
 print("Your URL is ready: ", s)
+
+root.mainloop()
