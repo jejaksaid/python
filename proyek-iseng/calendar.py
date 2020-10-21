37f9666ffd8e5b4514044537315e24debe4b8d00
jejaksaid <mail@saidramadhan.com>
2020-10-21 16:59:10 +0700

    2110

--- proyek-iseng/calendar.py
+++ proyek-iseng/calendar.py
@@ -1,87 +1,78 @@
-# import all methods and classes from the tkinter
+# importing all files from the tkinter
 from tkinter import *
-
-# import calendar module
+# importing calendar module
 import calendar
 
-
 # Function for showing the calendar of the given year
-def showCal():
+def showCal() :
+
     # Create a GUI window
-    new_gui = Tk()
-
+    root= Tk()
     # Set the background colour of GUI window
-    new_gui.config(background="white")
-
+    root.config(background = "white")
     # set the name of tkinter GUI window
-    new_gui.title("CALENDER")
-
+    root.title("MY CALENDAR")
     # Set the configuration of GUI window
-    new_gui.geometry("550x600")
-
-    # get method returns current text as string
+    root.geometry("650x700")
+    # using constructor int() we are type casting the string text that we through get() method
     fetch_year = int(year_field.get())
-
-    # calendar method of calendar module return
-    # the calendar of the given year .
-    cal_content = calendar.calendar(fetch_year)
+    # calendar() method of calendar module returns the calendar of the given year .
+    cal_data = calendar.calendar(fetch_year)
 
     # Create a label for showing the content of the calender
-    cal_year = Label(new_gui, text=cal_content, font="Consolas 10 bold")
+    cal_year = Label(root, text = cal_data, font = "Lucida 10 bold")
 
-    # grid method is used for placing
-    # the widgets at respective positions
-    # in table like structure.
-    cal_year.grid(row=5, column=1, padx=20)
+    # grid() method is used for placing the widgets at respective positions in table i.e 2-D like structure
+    cal_year.grid(row = 7, column = 1, padx = 20,)
 
     # start the GUI
-    new_gui.mainloop()
+    root.mainloop()
 
 
-# Driver Code
-if __name__ == "__main__":
+# Driver Code:
+#this mechanism ensures,the main function is executed only as direct run not when imported as module.
+if __name__ == "__main__" :
+
     # Create a GUI window
     gui = Tk()
 
     # Set the background colour of GUI window
-    gui.config(background="white")
+    gui.config(background = "light pink")
 
     # set the name of tkinter GUI window
-    gui.title("CALENDER")
+    gui.title("CALENDAR")
 
     # Set the configuration of GUI window
-    gui.geometry("250x140")
+    gui.geometry("250x250")
 
     # Create a CALENDAR : label with specified font and size
-    cal = Label(gui, text="CALENDAR", bg="dark gray",
-                font=("times", 28, 'bold'))
+    cal = Label(gui, text = "CALENDAR",bg="red",font = ("times", 28, 'bold'))
 
     # Create a Enter Year : label
-    year = Label(gui, text="Enter Year", bg="light green")
+    year = Label(gui, text = "Enter Year", bg = "light green")
 
     # Create a text entry box for filling or typing the information.
     year_field = Entry(gui)
 
     # Create a Show Calendar Button and attached to showCal function
-    Show = Button(gui, text="Show Calendar", fg="Black",
-                  bg="Red", command=showCal)
+    Show = Button(gui, text = "Show Calendar", fg = "Black",
+                              bg = "Red", command = showCal)
 
     # Create a Exit Button and attached to exit function
-    Exit = Button(gui, text="Exit", fg="Black", bg="Red", command=exit)
+    Exit = Button(gui, text = "Exit", bg = "Red", command = exit)
 
     # grid method is used for placing
     # the widgets at respective positions
     # in table like structure.
-    cal.grid(row=1, column=1)
+    cal.grid(row = 1, column = 1)
 
-    year.grid(row=2, column=1)
+    year.grid(row = 2, column = 1)
 
-    year_field.grid(row=3, column=1)
+    year_field.grid(row = 3, column = 1)
 
-    Show.grid(row=4, column=1)
+    Show.grid(row = 4, column = 1)
 
-    Exit.grid(row=6, column=1)
+    Exit.grid(row = 6, column = 1)
 
     # start the GUI
     gui.mainloop()
-
