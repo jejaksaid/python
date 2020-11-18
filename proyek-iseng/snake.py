2e666d7890a569c0204e56f6c920f81bf81b2d2e
jejaksaid <mail@saidramadhan.com>
2020-11-18 10:58:45 +0700

    1811

--- proyek-iseng/snake.py
+++ proyek-iseng/snake.py
@@ -60,6 +60,13 @@ class snake(object):
                 if i == len(self.body) -1:
                     self.turns.pop(p)
 
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: c.pos = (c.rows-1, c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0, c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = (c.pos[0], 0)
+                elif c.dirny == -1 and c.pos[1] <= 0: c.pos = (c.pos[0], c.rows-1)
+                else: c.move(c.dirnx, c.dirny)
+
     def reset(self,pos):
         pass
 
