b022e4bfde251cdb6e18521fc8e2203ce1c465ab
jejaksaid <mail@saidramadhan.com>
2020-11-25 23:59:31 +0700

    adding tutor 12-17

--- tutor17-optional-parameters.py
+++ tutor17-optional-parameters.py
@@ -10,11 +10,19 @@
 #     else:
 #         print('text is not 1')
 # func('x', '1')
+#
+# def func(x, text='1000'):
+#     print(x)
+#     if text == '1':
+#         print('text is 1')
+#     else:
+#         print(text)
+# func('x', '11')
 
-def func(x, text='1000'):
+def func(x=3, text='3'):
     print(x)
     if text == '1':
-        print('text is 1')
+        print ('text is 1')
     else:
-        print('text is not 1, but text is', text)
-func('x')
+        print(text)
+func()
