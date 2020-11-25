a480e8eb412bd8cff02d812475e913d49202a762
jejaksaid <mail@saidramadhan.com>
2020-11-25 23:27:00 +0700

    2511

--- tutor13-readtext.py
+++ tutor13-readtext.py
@@ -2,10 +2,11 @@ file = open('file.txt', 'r')
 f = file.readlines()
 
 newList = []
-# print(f)
+print(f)
 for line in f:
-    if line [-1] == '\n':
-        newList.append(line[:-1])
-    else:
-        newList.append()
+    # if line [-1] == '\n':
+    #     newList.append(line[:-1])
+    # else:
+    #     newList.append()
+    newList.append(line.strip())
 print(newList)
