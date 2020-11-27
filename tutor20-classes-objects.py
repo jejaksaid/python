b0d2ffc5edce8baa432b71f8f6abd73aa571d0ac
jejaksaid <mail@saidramadhan.com>
2020-11-27 23:54:36 +0700

    2711

--- tutor20-classes-objects.py
+++ tutor20-classes-objects.py
@@ -1,4 +1,12 @@
 x = 'string'
 y = 22
+boo = True
 # print(type(y))
-print(x.count('1'))
+# print(x.strip('1'))
+# print(x.count('1'))
+# print(boo.count('1')) its wrong
+# print(x.count())
+
+def func():
+    print('hello')
+func()
