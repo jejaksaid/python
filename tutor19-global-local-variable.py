37defebd30770b7d4ab1d734ae4707ae0de03866
jejaksaid <mail@saidramadhan.com>
2020-11-27 23:46:19 +0700

    2711

--- tutor19-global-local-variable.py
+++ tutor19-global-local-variable.py
@@ -1,12 +1,24 @@
 var = 9
 loop = True
 
+# def func(x):
+#     newVar = 7
+#     print(loop)
+#     if x == 5:
+#         return newVar
+# def otherFunc():
+#     newVar = 5
+#     print(newVar)
+# func(2)
 def func(x):
-    newVar = 7
-    print(loop)
+    global loop
+    loop = 7
+
     if x == 5:
         return newVar
 def otherFunc():
     newVar = 5
-    print(newVar)
-func(2)
+
+loop = False
+# func(2)
+print(loop)
