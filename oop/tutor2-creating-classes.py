f8cd1ecd7854b1df65e941972d680e9aeb59b5d1
jejaksaid <mail@saidramadhan.com>
2020-11-28 00:32:17 +0700

    2811

--- oop/tutor2-creating-classes.py
+++ oop/tutor2-creating-classes.py
@@ -2,6 +2,7 @@ class Cat(object):
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        self.li = [1,2,3]
 
     def speak(self):
         print("Hi I am", self.name, "and I am ", self.age, "years old")
@@ -9,9 +10,14 @@ class Cat(object):
     def change_age(self, age):
         self.age = age
 
+    def add_weight(self, weight):
+        self.weight = weight
+
 said = Cat('Said', 23)
 john = Cat('john', 39)
+said.add_weight(60)
+john.add_weight(50)
 said.change_age(22)
 said.speak()
 john.speak()
-print(said.name)
+print(john.weight)
