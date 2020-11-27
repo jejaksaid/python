c0f383bb9998751613e3165144046352c537001c
jejaksaid <mail@saidramadhan.com>
2020-11-28 00:15:23 +0700

    2811

--- oop/tutor1-object-oriented-programming.py
+++ oop/tutor1-object-oriented-programming.py
@@ -1,9 +1,11 @@
 import turtle
 x = 5
 y = 'string'
-
+f = 4.4
 said = turtle.Turtle()
 
+# print(f.upper()) float doesnt has upper method neither int
+print(y.replace('t', ''))
 def func(x):
     return x + 1
 print(func(5))
