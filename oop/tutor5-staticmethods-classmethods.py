06b8a8d9232c26b2f20fd33dd76520cc04e217b3
jejaksaid <mail@saidramadhan.com>
2020-12-01 00:01:48 +0700

    112

---
+++ oop/tutor5-staticmethods-classmethods.py
@@ -0,0 +1,20 @@
+class Dog:
+    dogs = []
+
+    def __init__(self, name):
+        self.name = name
+        self.dogs.append(self)
+
+    @classmethod
+    def num_dogs(cls):
+        return len(cls.dogs)
+
+# print(Dog.num_dogs()) #classmethod
+
+    @staticmethod
+    def bark(n):
+        """bark n times"""
+        for _ in range(n):
+            print("Bark!")
+            
+Dog.bark(10)
