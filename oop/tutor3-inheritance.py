5db9e6105172979b868fdbe853b3524b45a09cdc
jejaksaid <mail@saidramadhan.com>
2020-11-30 23:22:54 +0700

    3011

--- oop/tutor3-inheritance.py
+++ oop/tutor3-inheritance.py
@@ -6,5 +6,19 @@ class Dog(object):
     def speak(self):
         print("Hi I am", self.name, "and I am", self.age, "years old")
 
-said = Dog('Said', 11)
-said.speak()
+    def talk(self):
+        print('Bark!')
+
+class Cat(Dog): # parents child things
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
+
+    def talk(self):
+        print('Meow!')
+
+john = Dog('jogn', 30)
+john.talk()
+
+said = Cat('Said', 11, 'red')
+said.talk()
