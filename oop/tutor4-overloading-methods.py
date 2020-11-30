0dc5c7902728fa6fb1fd29fcecb635fab40d7443
jejaksaid <mail@saidramadhan.com>
2020-11-30 23:49:23 +0700

    3011

--- oop/tutor4-overloading-methods.py
+++ oop/tutor4-overloading-methods.py
@@ -17,6 +17,25 @@ class Point():
     def __mul__(self, p):
         return self.x * p.x + self.y * p.y
 
+    def length(self):
+        import math
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def __gt__(self, p):
+        return self.length() > p.length()
+
+    def __ge__(self, p):
+        return self.length() >= p.length()
+
+    def __lt__(self, p):
+        return self.length() < p.length()
+
+    def __le__(self, p):
+        return self.length() <= p.length()
+
+    def __eq__(self, p):
+        return self.x == p.x and self.y == p.y
+
     def __str__(self):
         return "(" + str(self.x) + ',' + str(self.y) + ")"
 
@@ -24,7 +43,11 @@ p1 = Point(3,4)
 p2 = Point(3,2)
 p3 = Point(1,3)
 p4 = Point(0,1)
-p5 = p1 + p2
-p6 = p4 - p1
-p7 = p2 * p3
-print(p5, p6, p7)
+# p5 = p1 + p2
+# p6 = p4 - p1
+# p7 = p2 * p3
+# print(p5, p6, p7)
+
+print(p1 == p2)
+print(p1 > p2)
+print(p4 <= p3)
